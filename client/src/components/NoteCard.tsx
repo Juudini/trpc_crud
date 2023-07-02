87d70fdf6dfce480f88ea082fbb4c1afbf65f521
Juan Debandi <juudinidev@gmail.com>
2023-07-02 20:49:55 -0300

    Improved styles

--- client/src/components/NoteCard.tsx
+++ client/src/components/NoteCard.tsx
@@ -9,45 +9,60 @@ export function NoteCard({ note }: Props) {
     const toggleDoneNote = trpc.note.toggleDone.useMutation();
     const utils = trpc.useContext();
 
-    return (
-        <div className="bg-sky-800 p-2 my-2 flex justify-between">
-            <div>
-                <h1 className="font-bold text-xl">{note.title}</h1>
-                <p>{note.description}</p>
-            </div>
-            <div className="flex gap-x-2">
-                <button
-                    onClick={() => {
-                        deleteNote.mutate(note._id, {
-                            onSuccess: (data) => {
-                                if (data) {
-                                    utils.note.get.invalidate();
-                                }
-                            },
-                            onError(error) {
-                                console.log(error);
-                            },
-                        });
-                    }}
-                    className="bg-pink-900 px-3 py-2 rounded-md text-white ml-auto">
-                    Delete
-                </button>
+    const handleDeleteNote = () => {
+        deleteNote.mutate(note._id, {
+            onSuccess: (data) => {
+                if (data) {
+                    utils.note.get.invalidate();
+                }
+            },
+            onError: (error) => {
+                console.log(error);
+            },
+        });
+    };
 
-                <button
-                    onClick={() => {
-                        toggleDoneNote.mutate(note._id, {
-                            onSuccess: (data) => {
-                                if (data) {
-                                    utils.note.get.invalidate();
-                                }
-                            },
-                        });
-                    }}
-                    className={`px-3 py-2 rounded-md text-white ml-auto ${
-                        note.done ? "bg-zinc-500" : "bg-sky-900"
-                    }`}>
-                    {note.done ? "Undone" : "Done"}
-                </button>
+    const handleToggleDoneNote = () => {
+        toggleDoneNote.mutate(note._id, {
+            onSuccess: (data) => {
+                if (data) {
+                    utils.note.get.invalidate();
+                }
+            },
+        });
+    };
+
+    return (
+        <div
+            className={`bg-gray-100 rounded-lg p-4 mb-4 shadow-md ${
+                note.done ? "bg-sky-100" : ""
+            }`}>
+            <div className="flex justify-between items-center">
+                <div>
+                    <h1
+                        className={`text-lg font-bold mb-2 ${
+                            note.done ? "line-through" : ""
+                        }`}>
+                        {note.title}
+                    </h1>
+                    <p className="text-gray-600">{note.description}</p>
+                </div>
+                <div className="flex gap-x-2">
+                    <button
+                        onClick={handleDeleteNote}
+                        className="px-3 py-2 rounded-md text-white bg-red-500 hover:bg-red-600 focus:bg-red-600 focus:outline-none">
+                        Delete
+                    </button>
+                    <button
+                        onClick={handleToggleDoneNote}
+                        className={`px-3 py-2 rounded-md text-white ${
+                            note.done
+                                ? "bg-green-500 hover:bg-green-600 focus:bg-green-600"
+                                : "bg-blue-500 hover:bg-blue-600 focus:bg-blue-600"
+                        } focus:outline-none`}>
+                        {note.done ? "Undone" : "Done"}
+                    </button>
+                </div>
             </div>
         </div>
     );
