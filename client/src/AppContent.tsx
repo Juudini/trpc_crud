9494b9701176ee500aa05e2e21afd245fb9aa6df
Juan Debandi <juudinidev@gmail.com>
2023-07-02 20:48:32 -0300

    Added Footer

--- client/src/AppContent.tsx
+++ client/src/AppContent.tsx
@@ -1,8 +1,11 @@
 import NotesList from "./components/NotesList";
 import NoteForm from "./components/NoteForm";
+import Footer from "./components/Footer";
+
 export default function AppContent() {
     return (
         <div className="max-w-xl m-auto h-screen py-40">
+            <Footer />
             <h1 className="text-5xl font-bold tex-center py-5"></h1>
             <NoteForm />
             <NotesList />
